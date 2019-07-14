3383613c93aeeb9de20c62275a0d492832c3e432
Kamil Nowak <kamilnowakx98@gmail.com>
2019-07-14 13:15:02 +0200

    change Button.js

--- src/component/Button.js
+++ src/component/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = props => {
   return (
     <button className={props.className} onClick={props.onClick}>
-      {props.text}
+      {props.children}
     </button>
   );
 };
