6cd6484fc8c59ae998b32bf969c23dea4796476a
Kamil Nowak <kamilnowakx98@gmail.com>
2019-09-06 09:20:59 +0200

    fix a problem with save property  doneDate in state.done

--- src/helperMethods/todayDate.js
+++ src/helperMethods/todayDate.js
@@ -1,7 +1,21 @@
 export default function todayDate() {
   const date = new Date().toLocaleDateString();
-  const day = date[0] + date[1];
-  const month = date[3] + date[4];
-  const year = date[6] + date[7] + date[8] + date[9];
+  console.log(date);
+  const newData = [];
+  let i = 0;
+  let name = "";
+  for (let j = 0; j < 3; j++) {
+    while (date[i] != "." && date.length > i) {
+      name += date[i];
+      i++;
+    }
+    i++;
+    newData.push(name);
+    name = "";
+  }
+  console.log(newData);
+  const day = newData[2];
+  const month = newData[1];
+  const year = newData[0];
   return `${year}-${month}-${day}`;
 }
